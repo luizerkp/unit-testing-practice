7efa8866b2ecdaa1c2e19bc198331374942b6b2f
Luis Tamarez <luistamarez@hotmail.com>
2023-02-09 00:57:47 -0800

    Add error handling for calculator   Add unit tests for substract and function   Add unit tests for divide and funtion   Refactor general error tests to a set of tests under one describe

--- src/calculator.js
+++ src/calculator.js
@@ -1,24 +1,123 @@
-const add = (num1, num2) => {
-  let result;
-  if (!num1 || !num2) {
-    result = "At least two numbers need to to perform addtion";
-  } else {
-    result = num1 + num2;
+const error = {
+  onlyOneNumber: "At least two numbers needed to to perform operation",
+  nonNumericValue: "At least one of your inputs contains a value that can not be converted to a number",
+  maxSafeIntValue: "At least one of your inputs is larger than Number.MAX_SAFE_INTERGER",
+  minSafeIntValue: "At least one of your inputs is smaller than Number.MIN_SAFE_INTERGER",
+};
+
+const isNumeric = (nums) =>
+  nums.every(
+    (num) => (typeof num === "number" || (typeof num === "string" && num.trim() !== "")) && !Number.isNaN(+num)
+  );
+
+const beyondMaxSafeIntValue = (nums) => {
+  if (nums.some((num) => num > Number.MAX_SAFE_INTEGER)) {
+    return true;
   }
+  return false;
+};
+
+const smallerThanMinSafeIntValue = (nums) => {
+  if (nums.some((num) => num < Number.MIN_SAFE_INTEGER)) {
+    return true;
+  }
+  return false;
+};
+
+const checkValidNumbers = (nums) => {
+  const valid = {
+    error: false,
+    errorMessage: "",
+  };
+
+  if (nums.length === 1 || nums.some((num) => !num)) {
+    valid.error = true;
+    valid.errorMessage = error.onlyOneNumber;
+    return valid;
+  }
+
+  if (!isNumeric(nums)) {
+    valid.error = true;
+    valid.errorMessage = error.nonNumericValue;
+    return valid;
+  }
+
+  if (smallerThanMinSafeIntValue(nums)) {
+    valid.error = true;
+    valid.errorMessage = error.minSafeIntValue;
+    return valid;
+  }
+
+  if (beyondMaxSafeIntValue(nums)) {
+    valid.error = true;
+    valid.errorMessage = error.maxSafeIntValue;
+    return valid;
+  }
+  return valid;
+};
+
+const add = (...nums) => {
+  const valid = checkValidNumbers(nums);
+
+  if (valid.error) {
+    throw new Error(valid.errorMessage);
+  }
+  const result = nums.reduce((total, current) => {
+    const currentNumber = +current;
+    let currentTotal = +total;
+    currentTotal += currentNumber;
+    return currentTotal;
+  });
   return result;
 };
 
-const subtract = () => {}; // TODO
+const subtract = (...nums) => {
+  const valid = checkValidNumbers(nums);
 
-const multiply = () => {}; // TODO
+  if (valid.error) {
+    throw new Error(valid.errorMessage);
+  }
+  const result = nums.reduce((total, current) => {
+    const currentNumber = +current;
+    let currentTotal = +total;
+    currentTotal -= currentNumber;
+    return currentTotal;
+  });
+  return result;
+};
 
-const divide = () => {}; // TODO
+const multiply = (...nums) => {
+  const valid = checkValidNumbers(nums);
+
+  if (valid.error) {
+    throw new Error(valid.errorMessage);
+  }
+  const result = nums.reduce((total, current) => {
+    const currentNumber = +current;
+    let currentTotal = +total;
+    currentTotal -= currentNumber;
+    return currentTotal;
+  });
+  return result;
+};
+
+const divide = (...nums) => {
+  const valid = checkValidNumbers(nums);
+
+  if (valid.error) {
+    throw new Error(valid.errorMessage);
+  }
+  // TODO
+};
 
 const calculator = {
   add,
   subtract,
   multiply,
   divide,
+  error,
 };
 
 export default calculator;
+
+// console.log(calculator.add("a", 2));
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,12 +3,240 @@ import calculator from "../src/calculator";
 test("Calculator object exist", () => {
   expect(typeof calculator).toBe("object");
 });
+
+describe("calculator Error Check Tests", () => {
+  test("Throws Error with only one num", () => {
+    expect(() => {
+      calculator.add(2);
+    }).toThrow(calculator.error.onlyOneNumber);
+    expect(() => {
+      calculator.subtract(2);
+    }).toThrow(calculator.error.onlyOneNumber);
+    expect(() => {
+      calculator.multiply(2);
+    }).toThrow(calculator.error.onlyOneNumber);
+    expect(() => {
+      calculator.divide(2);
+    }).toThrow(calculator.error.onlyOneNumber);
+  });
+
+  test("Throws Error with non-numbers (char)", () => {
+    expect(() => {
+      calculator.add("a", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.subtract("a", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.multiply("a", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.divide("a", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+  });
+
+  test("Throws Error with non-numbers (boolean)", () => {
+    expect(() => {
+      calculator.add(true, 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.subtract(true, 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.multiply(true, 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.divide(true, 2);
+    }).toThrow(calculator.error.nonNumericValue);
+  });
+
+  test("Throws Error with non-numbers (unicode)", () => {
+    expect(() => {
+      calculator.add("\u2103", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.subtract("\u2103", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.multiply("\u2103", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.divide("\u2103", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+  });
+
+  test("Throws Error with non-numbers (null)", () => {
+    expect(() => {
+      calculator.add(null, 2);
+    }).toThrow(calculator.error.onlyOneNumber);
+    expect(() => {
+      calculator.subtract(null, 2);
+    }).toThrow(calculator.error.onlyOneNumber);
+    expect(() => {
+      calculator.multiply(null, 2);
+    }).toThrow(calculator.error.onlyOneNumber);
+    expect(() => {
+      calculator.divide(null, 2);
+    }).toThrow(calculator.error.onlyOneNumber);
+  });
+
+  test("Throws Error with non-numbers (empty arr [])", () => {
+    expect(() => {
+      calculator.add([], 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.subtract([], 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.multiply([], 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.divide([], 2);
+    }).toThrow(calculator.error.nonNumericValue);
+  });
+
+  test("Throws Error with non-numbers (empty obj {})", () => {
+    expect(() => {
+      calculator.add({}, 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.subtract({}, 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.multiply({}, 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.divide({}, 2);
+    }).toThrow(calculator.error.nonNumericValue);
+  });
+
+  test("Throws Error with a string number with letters", () => {
+    expect(() => {
+      calculator.add("2a", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.subtract("2a", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.multiply("2a", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+    expect(() => {
+      calculator.divide("2a", 2);
+    }).toThrow(calculator.error.nonNumericValue);
+  });
+
+  test("Throws Error with a number larger than Number.MAX_SAFE_INTEGER", () => {
+    const num = Number.MAX_SAFE_INTEGER + 1;
+    expect(() => {
+      calculator.add(num, 2e3);
+    }).toThrow(calculator.error.maxSafeIntValue);
+    expect(() => {
+      calculator.subtract(num, 2e3);
+    }).toThrow(calculator.error.maxSafeIntValue);
+    expect(() => {
+      calculator.multiply(num, 2e3);
+    }).toThrow(calculator.error.maxSafeIntValue);
+    expect(() => {
+      calculator.divide(num, 2e3);
+    }).toThrow(calculator.error.maxSafeIntValue);
+  });
+
+  test("Throws Error with a number smaller than Number.MIN_SAFE_INTEGER", () => {
+    const num = Number.MIN_SAFE_INTEGER - 1;
+    expect(() => {
+      calculator.add(num, 2e3);
+    }).toThrow(calculator.error.minSafeIntValue);
+    expect(() => {
+      calculator.subtract(num, 2e3);
+    }).toThrow(calculator.error.minSafeIntValue);
+    expect(() => {
+      calculator.multiply(num, 2e3);
+    }).toThrow(calculator.error.minSafeIntValue);
+    expect(() => {
+      calculator.divide(num, 2e3);
+    }).toThrow(calculator.error.minSafeIntValue);
+  });
+});
+
 describe("Calculator Add tests", () => {
   test("add(a, b) returns a + b", () => {
     expect(calculator.add(2, 2)).toBe(4);
   });
 
-  test("Add handles only one num", () => {
-    expect(calculator.add(2)).toBe("At least two numbers need to to perform addtion");
+  test("Handles string numbers (ints)", () => {
+    expect(calculator.add("1", 2)).toBe(3);
+  });
+
+  test("Handles string numbers (floats)", () => {
+    expect(calculator.add("1.5", 2)).toBe(3.5);
+  });
+
+  test("Handles negative numbers", () => {
+    expect(calculator.add(-3, 2)).toBe(-1);
+  });
+
+  test("Handles floats", () => {
+    expect(calculator.add(2.5, 2)).toBe(4.5);
+  });
+
+  test("Handles scientific notation", () => {
+    expect(calculator.add(2e3, 2e3)).toBe(4e3);
+  });
+
+  test("Handles scientific notation and regular numbers", () => {
+    expect(calculator.add(2e3, 100)).toBe(2.1e3);
+  });
+
+  test("Handles string scientific notation", () => {
+    expect(calculator.add("2e3", 2e3)).toBe(4e3);
+  });
+
+  test("Handles more than two numbers", () => {
+    expect(calculator.add(1, 2, 3, 4)).toBe(10);
+  });
+  test("Handles more than two numbers with different polarities", () => {
+    expect(calculator.add(1, -2, 3, -4)).toBe(-2);
+  });
+});
+
+describe("Calculator subtract tests", () => {
+  test("subtract(a, b) returns a - b", () => {
+    expect(calculator.subtract(2, 2)).toBe(0);
+  });
+
+  test("Handles string numbers (ints)", () => {
+    expect(calculator.subtract("1", 2)).toBe(-1);
+  });
+
+  test("Handles string numbers (floats)", () => {
+    expect(calculator.subtract("1.5", 2)).toBe(-0.5);
+  });
+
+  test("Handles negative numbers", () => {
+    expect(calculator.subtract(-3, 2)).toBe(-5);
+  });
+
+  test("Handles floats", () => {
+    expect(calculator.subtract(2.5, 2)).toBe(0.5);
+  });
+
+  test("Handles scientific notation", () => {
+    expect(calculator.subtract(2e3, 2e3)).toBe(0);
+  });
+
+  test("Handles scientific notation and regular numbers", () => {
+    expect(calculator.subtract(2e3, 100)).toBe(1.9e3);
+  });
+
+  test("Handles string scientific notation", () => {
+    expect(calculator.subtract("2e3", 2e3)).toBe(0);
+  });
+
+  test("Handles more than two numbers", () => {
+    expect(calculator.subtract(1, 2, 3, 4)).toBe(-8);
+  });
+
+  test("Handles more than two numbers with different polarities", () => {
+    expect(calculator.subtract(1, -2, 3, -4)).toBe(4);
   });
 });
